cd0a2c3df843dd986e6a28f89749ff993e0352f3
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-09-09 09:39:26 -0300

    Configs docker updated

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,11 +2,16 @@ import { createConnection, getConnectionOptions } from 'typeorm';
 
 interface IOptions {
   host: string;
+  database: string;
 }
 
-getConnectionOptions().then(options => {
+getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
-  newOptions.host = 'localhost'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  newOptions.host = process.env.NODE_ENV === 'test' ? 'localhost' : 'database';
+  newOptions.database = process.env.NODE_ENV === 'test'
+    ? 'fin_api_test'
+    : 'fin_api';
+
   createConnection({
     ...options,
   });
